ba056357234ea25ecd8ffeba9b40cc5226ceaa69
BASILIO-S <basilioshae@gmail.com>
2025-10-22 11:53:59 +0800

    Frontend

---
+++ source/repos/G5-M2/ItemClientWinForms/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace ItemClientWinForms
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            dgvItems = new DataGridView();
+            btnAdd = new Button();
+            btnUpdate = new Button();
+            btnDelete = new Button();
+            btnRefresh = new Button();
+            txtName = new TextBox();
+            txtCode = new TextBox();
+            txtBrand = new TextBox();
+            txtUnitPrice = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dgvItems).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvItems
+            // 
+            dgvItems.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvItems.Location = new Point(21, 33);
+            dgvItems.Name = "dgvItems";
+            dgvItems.RowHeadersWidth = 51;
+            dgvItems.Size = new Size(754, 200);
+            dgvItems.TabIndex = 0;
+            dgvItems.SelectionChanged += dgvItems_SelectionChanged;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(346, 281);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(94, 29);
+            btnAdd.TabIndex = 1;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(346, 328);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(94, 29);
+            btnUpdate.TabIndex = 2;
+            btnUpdate.Text = "Update";
+            btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(516, 239);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(94, 29);
+            btnDelete.TabIndex = 3;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // btnRefresh
+            // 
+            btnRefresh.Location = new Point(660, 239);
+            btnRefresh.Name = "btnRefresh";
+            btnRefresh.Size = new Size(94, 29);
+            btnRefresh.TabIndex = 4;
+            btnRefresh.Text = "Refresh";
+            btnRefresh.UseVisualStyleBackColor = true;
+            btnRefresh.Click += btnRefresh_Click;
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(153, 280);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(125, 27);
+            txtName.TabIndex = 5;
+            // 
+            // txtCode
+            // 
+            txtCode.Location = new Point(153, 328);
+            txtCode.Name = "txtCode";
+            txtCode.Size = new Size(125, 27);
+            txtCode.TabIndex = 6;
+            // 
+            // txtBrand
+            // 
+            txtBrand.Location = new Point(153, 372);
+            txtBrand.Name = "txtBrand";
+            txtBrand.Size = new Size(125, 27);
+            txtBrand.TabIndex = 7;
+            // 
+            // txtUnitPrice
+            // 
+            txtUnitPrice.Location = new Point(153, 421);
+            txtUnitPrice.Name = "txtUnitPrice";
+            txtUnitPrice.Size = new Size(125, 27);
+            txtUnitPrice.TabIndex = 8;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(56, 283);
+            label1.Name = "label1";
+            label1.Size = new Size(49, 20);
+            label1.TabIndex = 9;
+            label1.Text = "Name";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(56, 331);
+            label2.Name = "label2";
+            label2.Size = new Size(44, 20);
+            label2.TabIndex = 10;
+            label2.Text = "Code";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(56, 379);
+            label3.Name = "label3";
+            label3.Size = new Size(48, 20);
+            label3.TabIndex = 11;
+            label3.Text = "Brand";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(56, 428);
+            label4.Name = "label4";
+            label4.Size = new Size(72, 20);
+            label4.TabIndex = 12;
+            label4.Text = "Unit Price";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 500);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtUnitPrice);
+            Controls.Add(txtBrand);
+            Controls.Add(txtCode);
+            Controls.Add(txtName);
+            Controls.Add(btnRefresh);
+            Controls.Add(btnDelete);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnAdd);
+            Controls.Add(dgvItems);
+            Name = "Form1";
+            Text = "Item Manager";
+            ((System.ComponentModel.ISupportInitialize)dgvItems).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dgvItems;
+        private Button btnAdd;
+        private Button btnUpdate;
+        private Button btnDelete;
+        private Button btnRefresh;
+        private TextBox txtName;
+        private TextBox txtCode;
+        private TextBox txtBrand;
+        private TextBox txtUnitPrice;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ source/repos/G5-M2/ItemClientWinForms/Form1.cs
@@ -0,0 +1,135 @@
+using ItemClientWinForms.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace ItemClientWinForms
+{
+    public partial class Form1 : Form
+    {
+        private readonly HttpClient _client;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _client = new HttpClient { BaseAddress = new Uri("https://localhost:7298/") };
+        }
+
+
+        // Load all items from API
+        private async Task LoadItems()
+        {
+            try
+            {
+                var items = await _client.GetFromJsonAsync<List<Item>>("api/items");
+                dgvItems.DataSource = items;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading items: {ex.Message}");
+            }
+        }
+
+        // Refresh button
+        private async void btnRefresh_Click(object sender, EventArgs e) => await LoadItems();
+
+        // Add button
+        private async void btnAdd_Click(object sender, EventArgs e)
+        {
+            var item = new Item
+            {
+                Name = txtName.Text,
+                Code = txtCode.Text,
+                Brand = txtBrand.Text,
+                UnitPrice = decimal.TryParse(txtUnitPrice.Text, out var p) ? p : 0
+            };
+
+            var resp = await _client.PostAsJsonAsync("api/items", item);
+            if (resp.IsSuccessStatusCode)
+            {
+                await LoadItems();
+                ClearFields();
+            }
+            elseSystem.Net.Http.HttpRequestException: 'No connection could be made because the target machine actively refused it. (localhost:7298)'
+
+            {
+                MessageBox.Show($"Error adding item: {resp.StatusCode}");
+            }
+        }
+
+        // Update button
+        private async void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (dgvItems.CurrentRow == null) return;
+            var item = (Item)dgvItems.CurrentRow.DataBoundItem;
+
+            item.Name = txtName.Text;
+            item.Code = txtCode.Text;
+            item.Brand = txtBrand.Text;
+            item.UnitPrice = decimal.TryParse(txtUnitPrice.Text, out var p) ? p : item.UnitPrice;
+
+            var resp = await _client.PutAsJsonAsync($"api/items/{item.Id}", item);
+            if (resp.IsSuccessStatusCode)
+            {
+                await LoadItems();
+                ClearFields();
+            }
+            else
+            {
+                MessageBox.Show($"Error updating item: {resp.StatusCode}");
+            }
+        }
+
+        // Delete button
+        private async void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dgvItems.CurrentRow == null) return;
+            var item = (Item)dgvItems.CurrentRow.DataBoundItem;
+
+            var resp = await _client.DeleteAsync($"api/items/{item.Id}");
+            if (resp.IsSuccessStatusCode)
+            {
+                await LoadItems();
+                ClearFields();
+            }
+            else
+            {
+                MessageBox.Show($"Error deleting item: {resp.StatusCode}");
+            }
+        }
+
+        // When selecting a row in DataGridView
+        private void dgvItems_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvItems.CurrentRow == null) return;
+            var item = (Item)dgvItems.CurrentRow.DataBoundItem;
+
+            txtName.Text = item.Name;
+            txtCode.Text = item.Code;
+            txtBrand.Text = item.Brand;
+            txtUnitPrice.Text = item.UnitPrice.ToString();
+        }
+
+        // Helper: clear textboxes
+        private void ClearFields()
+        {
+            txtName.Clear();
+            txtCode.Clear();
+            txtBrand.Clear();
+            txtUnitPrice.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
