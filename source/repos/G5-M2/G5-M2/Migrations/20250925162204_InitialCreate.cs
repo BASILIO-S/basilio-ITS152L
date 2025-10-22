a03179d58a96910682a0ec976cd6840e49e4117f
hanniohsovehnnova <162334131+hanniohsovehnnova@users.noreply.github.com>
2025-10-22 16:37:29 +0800

    Updated backend

--- source/repos/G5-M2/G5-M2/Migrations/20250925162204_InitialCreate.cs
+++ source/repos/G5-M2/G5-M2/Migrations/20250925162204_InitialCreate.cs
@@ -4,7 +4,7 @@
 
 #pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
 
-namespace BasilioM1.Migrations
+namespace G5M2.Migrations
 {
     /// <inheritdoc />
     public partial class InitialCreate : Migration
