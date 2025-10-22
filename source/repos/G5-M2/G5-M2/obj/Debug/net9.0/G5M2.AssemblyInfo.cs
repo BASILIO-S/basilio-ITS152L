79e328801dbb96d2a5dff1f8834e630dd39da50f
BASILIO-S <basilioshae@gmail.com>
2025-10-22 12:55:26 +0800

    adding all needed files

--- source/repos/G5-M2/G5-M2/obj/Debug/net9.0/G5M2.AssemblyInfo.cs
+++ source/repos/G5-M2/G5-M2/obj/Debug/net9.0/G5M2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("G5M2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e825829f0510a1a1d6a786ccbcf7a400e66f574e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+260d84c78bbffb5f4c4f9a80ab2da05ec556456e")]
 [assembly: System.Reflection.AssemblyProductAttribute("G5M2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("G5M2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
